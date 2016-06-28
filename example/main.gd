b6d571aed3bb604baf3184d7cc92d6137085de6a
Javier <jlopezcur@gmail.com>
2016-06-28 05:24:32 +0200

    Update main.gd

--- example/main.gd
+++ example/main.gd
@@ -3,11 +3,24 @@ extends Node
 var admob = null
 var isReal = true
 var isTop = true
-var ad_id = "ca-app-pub-XXXXXXXXXXXXXXXX/XXXXXXXXXX" # [Replace with your Ad Unit ID and delete this message.]
+var adId = "ca-app-pub-XXXXXXXXXXXXXXXX/XXXXXXXXXX" # [Replace with your Ad Unit ID and delete this message.]
 
 func _ready():
 	if(Globals.has_singleton("AdMob")):
 		admob = Globals.get_singleton("AdMob")
-		admob.init(isReal)
-		admob.loadBanner(ad_id, isTop)
+		admob.init(isReal, get_instance_ID())
 	
+func loadBanner():
+	if admob != null:
+		admob.loadBanner(adId, isTop)
+		get_tree().connect("screen_resized", self, "onResize")
+
+func onResize():
+	if admob != null:
+		admob.resize()
+
+func _on_admob_network_error():
+	print("Network Error")
+
+func _on_admob_ad_loaded():
+	print("Ad loaded success")
