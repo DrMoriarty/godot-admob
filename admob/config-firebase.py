840a681361c1dccc5f165bd76f77b3431ff6cc45
Javier López Úbeda <jlopezcur@gmail.com>
2016-11-05 06:24:17 +0100

    Firebase compatibility version

--- admob/config-firebase.py
+++ admob/config-firebase.py
@@ -3,7 +3,7 @@ def can_build(plat):
 
 def configure(env):
 	if (env['platform'] == 'android'):
-		env.android_add_dependency("compile 'com.google.firebase:firebase-ads:+'")
+		env.android_add_dependency("compile 'com.google.firebase:firebase-ads:9.2.1'")
 		env.android_add_java_dir("android")
 		env.android_add_to_manifest("android/AndroidManifestChunk.xml")
 		env.disable_module()
