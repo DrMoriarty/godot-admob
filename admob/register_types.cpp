31024525a97ec704a564b855b676121a43b435bd
Shin-NiL <niltonr@gmail.com>
2017-10-20 19:49:46 -0200

    Callbacks and Rewarded Videos implemented

--- admob/register_types.cpp
+++ admob/register_types.cpp
@@ -4,7 +4,7 @@
 #include "ios/src/godotAdmob.h"
 
 void register_admob_types() {
-    Globals::get_singleton()->add_singleton(Globals::Singleton("Admob", memnew(GodotAdmob)));
+    Globals::get_singleton()->add_singleton(Globals::Singleton("AdMob", memnew(GodotAdmob)));
 }
 
 void unregister_admob_types() {
